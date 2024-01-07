3c7d024abd5f7636205a43e8dec9b5a9a0cf3d84
MaxPowerz <christian.donohue@gmail.com>
2024-01-07 14:06:05 +1030

    Support for Chase Chase Jokers, UJK-001-2023121201
    This plugin will only work for version UJK-001-2023121201

---
+++ ccj@asphyixia/README.md
@@ -0,0 +1,4 @@
+# Chase Chase Jokers
+UJK-001-2023121201 
+Hardcoded responses for guest mode play.
+All maps unlocked for training mode.
---
+++ ccj@asphyixia/handlers/hardcode.ts
@@ -0,0 +1,36 @@
+export const saveResult: EPR = async (info, data, send) => {
+
+  send.xmlFile('xml/saveResult.xml');
+
+};
+
+export const pcbBoot: EPR = async (info, data, send) => {
+
+  send.xmlFile('xml/pcbBoot.xml');
+
+};
+
+export const getGachaSchedule: EPR = async (info, data, send) => {
+
+  send.xmlFile('xml/getGachaSchedule.xml');
+
+};
+
+export const getRankUpData: EPR = async (info, data, send) => {
+
+  send.xmlFile('xml/getRankUpData.xml');
+
+};
+
+export const getRanking: EPR = async (info, data, send) => {
+
+  send.xmlFile('xml/getRanking.xml');
+
+};
+
+export const dataLoad: EPR = async (info, data, send) => {
+
+  send.xmlFile('xml/dataLoad.xml');
+
+};
+
